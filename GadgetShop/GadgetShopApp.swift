9e9c931226145f7151345c8dc34ad8fe3e1a12c0
md.ibnesina <ibnesina10@gmail.com>
2023-11-19 08:12:03 +0600

    Initial Commit

---
+++ GadgetShop/GadgetShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GadgetShopApp.swift
+//  GadgetShop
+//
+//  Created by Md. Ibne Sina on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GadgetShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
