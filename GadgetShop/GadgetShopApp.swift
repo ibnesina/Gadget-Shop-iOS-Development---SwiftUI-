69aadc4627b2e2345f95f3a5f4a464a8b580175c
md.ibnesina <ibnesina10@gmail.com>
2023-11-28 19:18:57 +0600

    Project Completed

--- GadgetShop/GadgetShopApp.swift
+++ GadgetShop/GadgetShopApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct GadgetShopApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
